e4ead1b6c6ae6d6622abed54192005645c95d17f
Mehdi <jeyzermc@gmail.com>
2019-12-07 21:14:33 -0500

    MC: Random Forest 2

--- utils/precipitation.jl
+++ utils/precipitation.jl
@@ -1,1 +1,20 @@
-testerino() = "HELLO BONJOURNO"
+using Random, Dates;
+
+function findDistance(oLat, oLng, sLat, sLng)
+    lat = (sLat - oLat) ^ 2;
+    lng = (sLng - oLng) ^ 2;
+
+    return sqrt(lat + lng);
+end
+
+function normalize(val, min, max) 
+    return (val - min) / (max - min);
+end
+
+function shuffleDf(df)
+    sample_test = df[shuffle(1:size(df, 1)),:]
+end
+
+function dateToDay(dt)
+    return year(dt) * 365 + month(dt) * 30 + day(dt);
+end 
